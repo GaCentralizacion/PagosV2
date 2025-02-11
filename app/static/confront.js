2349a1a6623047774d3f7934393398a5959c1ace
Laura Gordillo <laura.gordillo.vargas@gmail.com>
2025-02-11 11:04:24 -0600

    se agrega url confront

--- app/static/confront.js
+++ app/static/confront.js
@@ -1,8 +1,11 @@
 var global_settings = {
 	urlCORS: 'http://192.168.20.123/PagosApi',
+	url : 'http://192.168.20.123',
 	//urlCORS: 'http://localhost:56669/',
 	urlNode: 'http://localhost:3600/api/',
+	// urlNode: 'http://' + window.location.hostname + ':3600/api/',
 	// urlNode: 'http://192.168.20.123:3600/api/',
+	urlNode: 'http://localhost:3600/api/',
 	urlMail: 'http://192.168.20.123:4010/',
 	liveReload: 1000
 }
